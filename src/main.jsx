61504fe982b04c3c68b49d8fb2b3634bef618736
Ascanio Roselli <ascanioroselli00@gmail.com>
2026-04-11 21:09:01 +0200

    fix Admin casing

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Admin from './admin'
-
+import Admin from './Admin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
