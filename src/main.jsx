6825a95af397b0face8443c27f351718e45dc847
Ascanio Roselli <ascanioroselli00@gmail.com>
2026-04-11 20:57:38 +0200

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Admin from './Admin'
-import './App.css'
+import Admin from './admin'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
